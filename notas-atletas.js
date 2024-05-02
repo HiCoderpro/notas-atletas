64a4491cbf6002521106869a577fdf0d2454c06d
Lucas Oliveira <lucasemfloripa@gmail.com>
2024-05-02 15:21:12 -0300

    Atualizando Atletas
    Fazendo instanciamento dos atletas

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,1 +1,32 @@
-console.log("Ola Programador!")
+//Criando Classe Atleta
+class Atleta {
+    constructor(nome, notas) {
+        this.nome = nome;
+        this.notas = notas
+    }obterNome(){
+        return this.nome
+    }
+
+    calcularMediaNota() {
+        let somaDasNotas = 0;
+
+        for (let i = 0; i < this.atletas.length; i++) {
+            somaDasNotas += this.atletas[i].nota;
+        }
+
+        const mediaNota = somaDasNotas / this.atletas.length;
+        return mediaNota;
+    }
+}
+
+
+
+//Lista de atletas abaixo
+let atletas = [
+    new Atleta("Cesar Abascal", [10, 9.34, 8.42, 10, 7.88]),
+    new Atleta("Fernando Puntel", [8, 10, 10, 7, 9.33]),
+    new Atleta("Daiane Jelinsky", [7, 10, 9.5, 9.5, 8]),
+    new Atleta("Bruno Castro", [10, 10, 10, 9, 9.5])
+   ];
+
+
