918a66c3989506f5ba682218a5a9604482bb7c91
Lucas Oliveira <lucasemfloripa@gmail.com>
2024-05-02 16:42:09 -0300

    Finalizando projeto

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,34 +1,45 @@
-//Criando Classe Atleta
 class Atleta {
     constructor(nome, notas) {
-        this.nome = nome;
-        this.notas = notas
-    }obterNome(){
-        return this.nome
-    }obterNotasOrdenada(){
-        return this.notas.slice().sort((a, b) => a - b)
+      this.nome = nome;
+      this.notas = notas;
+    }
+  
+    //Pegamos o nome dos atletas
+    obterNome() {
+      return this.nome;
+    }
+    //Obtemos nossa lista em ordem ordenadas ou seja do menor ao maior!
+    obterNotasOrdenadas() {
+      return this.notas.slice().sort((a, b) => a - b);
+    }
+  
+    obterNotasValidas() {
+      const notasOrdenadas = this.obterNotasOrdenadas();
+      return notasOrdenadas.slice(1, 4);
+    }
+    
+    obterMediaValida() {
+      const notasValidas = this.obterNotasValidas();
+      return notasValidas.reduce((soma, nota) => soma + nota, 0) / notasValidas.length;
     }
 
-    calcularMediaNota() {
-        let somaDasNotas = 0;
-
-        for (let i = 0; i < this.atletas.length; i++) {
-            somaDasNotas += this.atletas[i].nota;
-        }
-
-        const mediaNota = somaDasNotas / this.atletas.length;
-        return mediaNota;
-    }
-}
-
-
-
-//Lista de atletas abaixo
-let atletas = [
+  }
+  
+  // Array de objetos de atletas
+  const atletas = [
     new Atleta("Cesar Abascal", [10, 9.34, 8.42, 10, 7.88]),
     new Atleta("Fernando Puntel", [8, 10, 10, 7, 9.33]),
     new Atleta("Daiane Jelinsky", [7, 10, 9.5, 9.5, 8]),
     new Atleta("Bruno Castro", [10, 10, 10, 9, 9.5])
-   ];
+  ];
+  
+  // Exemplo de uso
+  for (let i = 0; i < atletas.length; i++) {
+    const primeiroAtleta = atletas[i];
+    console.log(`Atleta: `,primeiroAtleta.obterNome());
+    console.log(`Notas Obtidas: `, primeiroAtleta.obterNotasOrdenadas());
+    console.log(`Média Válida: `,primeiroAtleta.obterMediaValida());
+    console.log(`        `)
+}
 
-
+  
